8e37b5f8f945f8a4e462eb4f30c18b14fe0a13c7
suchit <suchitbajracharya1@gmail.com>
2025-03-18 20:49:55 +0545

    tested and nested folder bug fix

--- auto_LogicCode.py
+++ auto_LogicCode.py
@@ -4,7 +4,12 @@ path = r"C:/Users/ACER/Desktop/test/"
 
 #checking if sorting folder exists
 type=["Document","Image","Diagram"]
-
+# things=os.listdir(path)
+new=[]
+#filter files only
+for files in os.listdir(path):
+    if not os.path.isdir(path+files):
+        new.append(files)
 #looping to find if folders like that exists 
 # othwerwise make it
 for i in range(0,len(type)):
@@ -12,12 +17,19 @@ for i in range(0,len(type)):
         os.makedirs(path+type[i])
 
 #now sorting it in respective file
-for file in os.listdir(path):
+for file in new:
 #check extension in name of file and if path inside desired foolder exist
     if (".doc" in file or ".pdf" in file) and not os.path.exists(path +"Document/"+file):
     #moves the file insde desired folder
         shutil.move(path + file, path +"Document/"+file)
 #check extension in name of file and if path inside desired foolder exist
-    elif (".jpg" or ".png" or ".jpeg" in file) and not os.path.exist(path+"Image/"+file):
+    if (".jpg" in file or ".png" in file or ".jpeg" in file) and not os.path.exists(path+"Image/"+file):
     #moves the file insde desired folder
         shutil.move(path+file, path+"Image/"+file) 
+    
+#check extension in name of file and if path inside desired foolder exist
+    if ".drawio" in file and not os.path.exists(path+"Diagram/"+file):
+    #moves the file insde desired folder
+        shutil.move(path+file, path+"Diagram/"+file) 
+    
+        
