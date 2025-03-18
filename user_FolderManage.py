c1d893dd871cc61ef91f94c2ba38561900c44bcb
suchit <suchitbajracharya1@gmail.com>
2025-03-18 22:00:06 +0545

    login added for safety not completed

--- user_FolderManage.py
+++ user_FolderManage.py
@@ -5,9 +5,13 @@ path=r""
 #creating window for user
 root=Tk()
 root.config(bg="#f0f0f0")
-root.geometry("500x700+0+0")
+root.geometry("500x500+400+0")
 root.resizable(0,0)
 #functions--------------------------------------------------
+def verify():
+    usr=user.get()
+    pas=pwd.get()
+    print(usr,pas)
 def file_window():
     global path
     find=filedialog.askdirectory() 
@@ -15,15 +19,32 @@ def file_window():
     path=find+"/"
     print(path)
 #------------------------------------------------------------
+#login
+
+frame=Frame(root)
+#inside frame
+a=Label(frame,text="username",font=("Helvetica", 10), fg="#333", bg="#f0f0f0")
+user=Entry(frame)
+b=Label(frame,text="password",font=("Helvetica", 10), fg="#333", bg="#f0f0f0")
+pwd=Entry(frame)
+#widget place
+Label(frame,text="Login",font=("Helvetica", 18, "bold"), fg="#333", bg="#f0f0f0").grid(row=0,column=1)
+Button(frame,text="submit",command=verify,font=("Helvetica", 8), fg="#333", bg="#f0f0f0").grid(row=3,column=1)
+a.grid(row=1,column=0)
+user.grid(row=1,column=1)
+b.grid(row=2,column=0)
+pwd.grid(row=2,column=1)
+frame.pack()
+#database
 
 
 #GUI---------------------------------------------------------
 #labels
-heading=Label(root,text="FILE MANAGER",font=("Helvetica", 18, "bold"), fg="#333", bg="#f0f0f0")
-folder_finding=Button(text="choose folder",command=file_window)
+# heading=Label(root,text="FILE MANAGER",font=("Helvetica", 18, "bold"), fg="#333", bg="#f0f0f0")
+# folder_finding=Button(text="choose folder",command=file_window)
 
-heading.pack()
-folder_finding.pack()
+# heading.pack()
+# folder_finding.pack()
 
 
 
