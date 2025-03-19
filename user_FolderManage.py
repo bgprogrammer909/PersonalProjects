6ee8acd738a52fd43ad67b82ce0e7c67dfd31f82
suchit <suchitbajracharya1@gmail.com>
2025-03-19 21:45:13 +0545

    login and pop up

--- user_FolderManage.py
+++ user_FolderManage.py
@@ -1,17 +1,33 @@
 #importing required modules
 from tkinter import *
 from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import Toplevel
 path=r""
 #creating window for user
 root=Tk()
 root.config(bg="#f0f0f0")
 root.geometry("500x500+400+0")
 root.resizable(0,0)
+att=0
 #functions--------------------------------------------------
 def verify():
+    global att
     usr=user.get()
     pas=pwd.get()
-    print(usr,pas)
+    if usr=="Suchit" and pas=="pythonisbest":
+        root.destroy()
+        att=0
+        roots=Toplevel()
+        roots.config(bg="#f0f0f0")
+        roots.geometry("500x500+400+0")
+        roots.resizable(0,0)
+    else:
+        att+=1
+        messagebox.showerror("oops!",f"invalid username or password,{3-att} attempts left")
+        if att==3:
+            root.destroy()
+        
 def file_window():
     global path
     find=filedialog.askdirectory() 
